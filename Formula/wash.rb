db6a6a4ea7ab114edcec2d418c6a3719cac4d9e7
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-01-19 08:11:51 -0700

    Bump Version to 0.1.14

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,8 +1,8 @@
 class Wash < Formula
   desc "WAsmcloud SHell - A multi-tool for various wasmCloud activities as well as an interactive REPL for a wasmCloud runtime host."
   homepage "https://wascc.dev"
-  url "https://github.com/wasmCloud/wash/archive/v0.1.11.tar.gz"
-  sha256 "606bf6272f6b8fe3ee4c3c23747162b5432b1bdec176db3f0796190a19886863"
+  url "https://github.com/wasmCloud/wash/archive/v0.1.14.tar.gz"
+  sha256 "13ec345a4467d6f734e74035ac2e64abbe82063ee59ead00d575877b5e2ff012"
   license "Apache-2.0"
 
   depends_on "rust" => :build
