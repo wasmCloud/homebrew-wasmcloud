2f442691db72ae09c38b8b5cc0bb05590898f186
Bill Young <bill@byoung.io>
2021-02-17 11:55:37 -0500

    Release: wasmcloud v0.15.0

---
+++ Formula/wasmcloud.rb
@@ -0,0 +1,18 @@
+class WasmCloud < Formula
+  desc "A secure, distributed, WebAssembly actor model runtime for the cloud, edge, and everything in between"
+  homepage "https://wasmcloud.dev"
+  url "https://github.com/wasmCloud/wasmCloud/archive/v0.15.0.tar.gz"
+  sha256 "c99aaafdaf033140255c5350aaa845a2a172a7c2a0a9c667f38905457aebcdf0"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "-q", "--release", "--bin", "wasmcloud"
+    bin.install "target/release/wasmcloud"
+  end
+
+  test do
+    system "wasmcloud", "-V"
+  end
+end
