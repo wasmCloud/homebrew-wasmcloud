6ad0f1fb50ca523268fb31777078158b9525cdfd
Bill Young <bill@byoung.io>
2021-02-17 11:23:05 -0500

    Release: wash v0.2.0

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,8 +1,8 @@
 class Wash < Formula
   desc "WAsmcloud SHell - A multi-tool for various wasmCloud activities as well as an interactive REPL for a wasmCloud runtime host."
   homepage "https://wascc.dev"
-  url "https://github.com/wasmCloud/wash/archive/v0.1.14.tar.gz"
-  sha256 "13ec345a4467d6f734e74035ac2e64abbe82063ee59ead00d575877b5e2ff012"
+  url "https://github.com/wasmCloud/wash/archive/v0.2.0.tar.gz"
+  sha256 "80ef40f4f069eeb08a45ade5817ae18d665bbadb3f39ef937f480ddb1e994212"
   license "Apache-2.0"
 
   depends_on "rust" => :build
