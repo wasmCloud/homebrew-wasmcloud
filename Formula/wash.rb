257549405dcf5fd89ea87f2feee81c895b3c6e63
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-03-11 19:46:45 -0700

    update homepage

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,6 +1,6 @@
 class Wash < Formula
   desc "WAsmcloud SHell - A multi-tool for various wasmCloud activities as well as an interactive REPL for a wasmCloud runtime host."
-  homepage "https://wascc.dev"
+  homepage "https://wasmcloud.dev"
   url "https://github.com/wasmCloud/wash/archive/v0.3.0.tar.gz"
   sha256 "f1db12c055cc05677a6c2bab174bbacbb20cc4e5305412b662f982e5ba2a6884"
   license "Apache-2.0"
