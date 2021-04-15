c6d11b8ec5ff41a4e7e266c9dbeed1dc705f0a6f
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2021-04-15 09:07:45 -0600

    Bump 0.17.0

--- Formula/wasmcloud.rb
+++ Formula/wasmcloud.rb
@@ -1,8 +1,8 @@
 class Wasmcloud < Formula
   desc "A secure, distributed, WebAssembly actor model runtime for the cloud, edge, and everything in between"
   homepage "https://wasmcloud.dev"
-  url "https://github.com/wasmCloud/wasmCloud/archive/v0.16.0.tar.gz"
-  sha256 "99ad0219b1ac23d6ce0956c4527dfde08b8e6775d9cda471ee389ba1ca0c2c54"
+  url "https://github.com/wasmCloud/wasmCloud/archive/v0.17.0.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "Apache-2.0"
 
   depends_on "rust" => :build
