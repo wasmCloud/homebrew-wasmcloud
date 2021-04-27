161ed852366a35868a54a70946b282231cb9085a
Brooks Townsend <brooksmtownsend@gmail.com>
2021-04-27 11:36:28 -0400

    bump 0.4.3

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,7 +1,7 @@
 class Wash < Formula
   desc "WAsmcloud SHell - A multi-tool for various wasmCloud activities as well as an interactive REPL for a wasmCloud runtime host."
   homepage "https://wasmcloud.dev"
-  url "https://github.com/wasmCloud/wash/archive/v0.4.2.tar.gz"
+  url "https://github.com/wasmCloud/wash/archive/v0.4.3.tar.gz"
   sha256 "c4c1e4f6b6821903c51f3a23068593125d470bebfd5dd779ac3071a37ec3a1a3"
   license "Apache-2.0"
 
