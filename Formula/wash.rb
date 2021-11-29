68ad5b93e1a6732cfc7bbb457bec7590da5bba62
Brooks Townsend <brooksmtownsend@gmail.com>
2021-11-29 11:53:36 -0500

    update wash to v0.7.1
    Signed-off-by: Brooks Townsend <brooks@mbp16inch2021.lan>

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,8 +1,8 @@
 class Wash < Formula
   desc "WAsmcloud SHell - A multi-tool for various wasmCloud activities as well as an interactive REPL for a wasmCloud runtime host."
   homepage "https://wasmcloud.dev"
-  url "https://github.com/wasmCloud/wash/archive/v0.7.0.tar.gz"
-  sha256 "7214635688aae4ae06755fd482c767f5db4571ac9d82c0141d02eab34ba64298"
+  url "https://github.com/wasmCloud/wash/archive/v0.7.1.tar.gz"
+  sha256 "c2602a85e9745b33145aeaad12957241764bd7d213a77355934cb827f289935a"
   license "Apache-2.0"
 
   depends_on "rust" => :build
