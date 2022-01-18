63a8fcb1b2e7b85c394c5926c017fba67e1ce30f
Brooks Townsend <brooksmtownsend@gmail.com>
2022-01-18 11:38:13 -0500

    updated wash to 0.8.0

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,8 +1,8 @@
 class Wash < Formula
   desc "WAsmcloud SHell - A multi-tool for various wasmCloud activities as well as an interactive REPL for a wasmCloud runtime host."
   homepage "https://wasmcloud.dev"
-  url "https://github.com/wasmCloud/wash/archive/v0.7.2.tar.gz"
-  sha256 "45e94be657e2906dc6582fa1f1a73d3a8ad494926d0f71eda6a12c094635b44e"
+  url "https://github.com/wasmCloud/wash/archive/v0.8.0.tar.gz"
+  sha256 "4c8f1153484dbc47cec93278410bd3e62f1b0f2a3c46f22102d0caec2ca4b3b0"
   license "Apache-2.0"
 
   depends_on "rust" => :build
