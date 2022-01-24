1d4d698f29ed76ad1c21c03cfe62b646c21de101
Taylor Thomas <taylor@cosmonic.com>
2022-01-24 12:16:12 -0700

    fix(*): addresses brew audits
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,5 +1,5 @@
 class Wash < Formula
-  desc "WAsmcloud SHell - A multi-tool for various wasmCloud activities as well as an interactive REPL for a wasmCloud runtime host."
+  desc "WAsmcloud SHell - A multi-tool for various wasmCloud interactions"
   homepage "https://wasmcloud.dev"
   url "https://github.com/wasmCloud/wash/archive/v0.8.0.tar.gz"
   sha256 "4c8f1153484dbc47cec93278410bd3e62f1b0f2a3c46f22102d0caec2ca4b3b0"
@@ -8,8 +8,7 @@ class Wash < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "-q", "--release", "--bin", "wash"
-    bin.install "target/release/wash"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
--- Formula/wasmcloud.rb
+++ Formula/wasmcloud.rb
@@ -1,5 +1,5 @@
 class Wasmcloud < Formula
-  desc "A secure, distributed, WebAssembly actor model runtime for the cloud, edge, and everything in between"
+  desc "Secure, distributed, WebAssembly actor model runtime for everywhere"
   homepage "https://wasmcloud.dev"
   url "https://github.com/wasmCloud/wasmCloud/archive/v0.18.2.tar.gz"
   sha256 "6e393db103047909ee5295abbf95d460614b1a4dbca53fc9dda305451c7b1844"
@@ -8,8 +8,7 @@ class Wasmcloud < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "-q", "--release", "--bin", "wasmcloud"
-    bin.install "target/release/wasmcloud"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
