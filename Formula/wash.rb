96c2d201d0eda51a4b056230ebdc34287f973334
Brooks Townsend <brooks@cosmonic.com>
2022-01-31 23:30:09 +0000

    wash: update 0.9.0 bottle.

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -6,10 +6,9 @@ class Wash < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/wasmCloud/homebrew-wasmcloud/releases/download/wash-0.8.0"
-    rebuild 1
-    sha256 cellar: :any,                 big_sur:      "671c2d0c882056d1e31b8db4a3fec76a20e2f7a72d0e0b95dd763ab90d421343"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3523c79ed645aacfdc32ba4ef728c26caf88b3e465e1cc428dff6c341ca1962e"
+    root_url "https://github.com/wasmCloud/homebrew-wasmcloud/releases/download/wash-0.9.0"
+    sha256 cellar: :any,                 big_sur:      "59505a1bf398032fcc422b4f611697c4a9936cf81e77ad26e6aac0129aca267b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "00855b6e454dba59c43f6135a3370f86e788c8a36f83ab55013ee74e95ddbc9f"
   end
 
   depends_on "rust" => :build
