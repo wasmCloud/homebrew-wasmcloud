61abf5a1c0d660499476b9fb681f07aa694c5127
Connor Smith <3869136+connorsmith256@users.noreply.github.com>
2023-03-14 01:53:56 +0000

    wash: update 0.16.1 bottle.

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -6,9 +6,9 @@ class Wash < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/wasmCloud/homebrew-wasmcloud/releases/download/wash-0.15.1"
-    sha256 cellar: :any_skip_relocation, big_sur:      "3f629847a16c9b089f8a947bc27875d18414b71172fbeacec8da88c19d538942"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "243e06d7f14370ab9a14db8e357767e3eb3416e83041ba6aaada9d83e32b3d3f"
+    root_url "https://github.com/wasmCloud/homebrew-wasmcloud/releases/download/wash-0.16.1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "b5c15d1208bfb6c77009e0c99678a6764c3d7f9c669ad33b153cc0cf65fd6e99"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8b5c1d99f8a007419dc9e46dd777db523ee5b1312909f97e01920d3c6c8785df"
   end
 
   depends_on "rust"
