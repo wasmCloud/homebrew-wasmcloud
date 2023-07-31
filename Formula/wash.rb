c7314e9189f67e7b3034cba056399bd4274c3506
brooksmtownsend <brooksmtownsend@users.noreply.github.com>
2023-07-31 14:07:51 +0000

    bump wash to v0.19.1
    Closes #45.
    
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,14 +1,14 @@
 class Wash < Formula
   desc "WAsmcloud SHell - a comprehensive command-line tool for wasmCloud development"
   homepage "https://wasmcloud.com"
-  url "https://github.com/wasmCloud/wash/archive/v0.19.0.tar.gz"
-  sha256 "d752df663c1b1e9dca38588a05080a25f5d6cdf3cb8c5a6a41f207c53f85e560"
+  url "https://github.com/wasmCloud/wash/archive/v0.19.1.tar.gz"
+  sha256 "ca635c1163c4a5b9b288021455d06578c09a3e2e8db4726a3954ce887243f7ee"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/wasmCloud/homebrew-wasmcloud/releases/download/wash-0.19.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "1f1619182085dafb76edc2915a90b9f02e847154b4288b79a779656d874ed060"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "01965823ed432c07d3461e360f12b46076427af102f949bade91a9d97286f362"
+    root_url "https://github.com/wasmCloud/homebrew-wasmcloud/releases/download/wash-0.17.4"
+    sha256 cellar: :any_skip_relocation, big_sur:      "6779804593d697ac4b43c501c45ebe04ce3f42cac1e2f9f89f481d20ac5f9dac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5c202386e8f361a790f5416193b38888ab18453826f52db74d691c09afbe3e3f"
   end
 
   depends_on "rust"
