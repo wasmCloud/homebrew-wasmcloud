807363e86274109aeb6f820b15ea9526c683af42
Joonas Bergius <joonas@cosmonic.com>
2023-11-14 11:11:41 -0600

    Address audit change introduced in 4.1.18
    Signed-off-by: Joonas Bergius <joonas@cosmonic.com>

--- Formula/wasmcloud.rb
+++ Formula/wasmcloud.rb
@@ -1,7 +1,7 @@
 class Wasmcloud < Formula
   desc "Secure, distributed, WebAssembly actor model runtime for everywhere"
   homepage "https://wasmcloud.dev"
-  url "https://github.com/wasmCloud/wasmCloud/archive/v0.18.2.tar.gz"
+  url "https://github.com/wasmCloud/wasmCloud/archive/refs/tags/v0.18.2.tar.gz"
   sha256 "6e393db103047909ee5295abbf95d460614b1a4dbca53fc9dda305451c7b1844"
   license "Apache-2.0"
 
