4c044eb9caa1441419cafbc7f5d145a6c7258bb9
automation-wasmcloud <automation-wasmcloud@users.noreply.github.com>
2026-06-03 01:49:53 +0000

    bump wash to v2.3.0
    Signed-off-by: automation-wasmcloud <automation-wasmcloud@users.noreply.github.com>

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -8,16 +8,16 @@ class Wash < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.2.1/wash-x86_64-apple-darwin"
-      sha256 "5d912090d05b956f73a6c83366915a257dcffc1ed6fad86c19f254890917bfa8"
+      url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.3.0/wash-x86_64-apple-darwin"
+      sha256 "d794a286244b3499c3975c49f3ef762b685792df7e0b2c1043a542820498a860"
 
       define_method(:install) do
         bin.install "wash-x86_64-apple-darwin" => "wash"
       end
     end
     on_arm do
-      url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.2.1/wash-aarch64-apple-darwin"
-      sha256 "4287ad3041ecbb572e0a190d085fba5c24e645b2ab3bee06fbcae17c438d508e"
+      url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.3.0/wash-aarch64-apple-darwin"
+      sha256 "7ca11fcfa95c7051d618eb2f30becd5f5999429e97fa45789bda4622999b1cf7"
 
       define_method(:install) do
         bin.install "wash-aarch64-apple-darwin" => "wash"
@@ -28,8 +28,8 @@ class Wash < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.2.1/wash-x86_64-unknown-linux-gnu"
-        sha256 "5cf94680e5f6f31429fbdd6763f52d08cacd062becc3715dd15353541da9f50d"
+        url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.3.0/wash-x86_64-unknown-linux-gnu"
+        sha256 "8b54c5516737e918aabc61ccc80992c06b1feeda6069d3405c23b09ddff5f6a8"
 
         define_method(:install) do
           bin.install "wash-x86_64-unknown-linux-gnu" => "wash"
@@ -38,8 +38,8 @@ class Wash < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.2.1/wash-aarch64-unknown-linux-gnu"
-        sha256 "b72cf54f17137d3288acf420ec44836e9c1f5372bce6d6a29b951099fd89f15b"
+        url "https://github.com/wasmCloud/wasmCloud/releases/download/v2.3.0/wash-aarch64-unknown-linux-gnu"
+        sha256 "17196d72c0464d60d34a195044c167d4aae41b5d51c419ea1ddb9ab1c5c8bcb4"
 
         define_method(:install) do
           bin.install "wash-aarch64-unknown-linux-gnu" => "wash"
